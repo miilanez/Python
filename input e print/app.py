b80a30bfcdb367aa44411c5a92a82631ff3bdb3a
miilanez <gabrielmilanez99@hotmail.com>
2024-12-23 09:39:03 -0300

    ft: input e print

---
+++ input e print/app.py
@@ -0,0 +1,6 @@
+#Input
+
+nome = input("Digite seu nome: ")
+idade = int(input("Digite sua Idade: "))
+altura = float(input("Digite altura (Ex 1.81): "))
+print (f"Seu nome é:", nome , ", você tem", altura , "m e" , idade , "anos")
